3e5356cdf45b7ac52898da24be040626bd184036
Kevin Smith <kevin.smith@isode.com>
2015-11-21 20:45:13 +0000

    Update to use C++11 features instead of boost to match librestpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,12 @@
  * See the LICENSE file for more information.
  */
 
+#include <memory>
 #include <string>
 #include <vector>
 
 #include <boost/bind.hpp>
 #include <boost/lexical_cast.hpp>
-#include <boost/make_shared.hpp>
-#include <boost/shared_ptr.hpp>
 #include <boost/thread.hpp>
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_io.hpp>
@@ -36,20 +35,20 @@ class DemoSession {
 		WebSocket::ref webSocket_;
 };
 
-class DemoSessionCollection : public SessionCollection<boost::shared_ptr<DemoSession>> {
+class DemoSessionCollection : public SessionCollection<std::shared_ptr<DemoSession>> {
 	public:
 		DemoSessionCollection() {}
 		virtual ~DemoSessionCollection() {}
 
-		std::string addSession(boost::shared_ptr<DemoSession> session) {
+		std::string addSession(std::shared_ptr<DemoSession> session) {
 			std::string key = boost::lexical_cast<std::string>(uuidGenerator_());
 			sessions_[key] = session;
-			webSocketSessions_[session->webSocket_] = std::pair<boost::shared_ptr<DemoSession>, std::string>(session, key);
+			webSocketSessions_[session->webSocket_] = std::pair<std::shared_ptr<DemoSession>, std::string>(session, key);
 			return key;
 		}
 
 		void removeSession(WebSocket::ref webSocket) {
-			boost::shared_ptr<DemoSession> session = webSocketSessions_[webSocket].first;
+			std::shared_ptr<DemoSession> session = webSocketSessions_[webSocket].first;
 			std::string key = webSocketSessions_[webSocket].second;
 			if (!session) return;
 			sessions_.erase(key);
@@ -60,22 +59,22 @@ class DemoSessionCollection : public SessionCollection<boost::shared_ptr<DemoSes
 			return webSocketSessions_.count(webSocket) > 0;
 		}
 
-		virtual boost::shared_ptr<DemoSession> getSession(const std::string& sessionKey) {
+		virtual std::shared_ptr<DemoSession> getSession(const std::string& sessionKey) {
 			return sessions_[sessionKey];
 		}
 	private:
-		std::map<WebSocket::ref, std::pair<boost::shared_ptr<DemoSession>, std::string>> webSocketSessions_;
+		std::map<WebSocket::ref, std::pair<std::shared_ptr<DemoSession>, std::string>> webSocketSessions_;
 		boost::uuids::random_generator uuidGenerator_;
-		std::map<std::string, boost::shared_ptr<DemoSession>> sessions_;
+		std::map<std::string, std::shared_ptr<DemoSession>> sessions_;
 };
 
-boost::shared_ptr<JSONObject> configToJSON(Config* config) {
-	boost::shared_ptr<JSONObject> json = boost::make_shared<JSONObject>();
-	json->set("name", boost::make_shared<JSONString>(config->name_));
+std::shared_ptr<JSONObject> configToJSON(Config* config) {
+	std::shared_ptr<JSONObject> json = std::make_shared<JSONObject>();
+	json->set("name", std::make_shared<JSONString>(config->name_));
 	return json;
 }
 
-void handleConfigGetRequest(boost::shared_ptr<RESTRequest> request, Config* config) {
+void handleConfigGetRequest(std::shared_ptr<RESTRequest> request, Config* config) {
 	request->setReplyHeader(RESTRequest::HTTP_OK);
 	request->addReplyContent(configToJSON(config));
 
@@ -85,7 +84,7 @@ void handleConfigGetRequest(boost::shared_ptr<RESTRequest> request, Config* conf
 
 class WebSockets {
 	public:
-		WebSockets(boost::shared_ptr<DemoSessionCollection> sessions) : sessions_(sessions) {}
+		WebSockets(std::shared_ptr<DemoSessionCollection> sessions) : sessions_(sessions) {}
 		void handleNewWebSocket(WebSocket::ref webSocket) {
 			std::cout << "Received new websocket connection" << std::endl;
 			webSocket->onMessage.connect(boost::bind(&WebSockets::handleMessage, this, webSocket, _1));
@@ -97,21 +96,21 @@ class WebSockets {
 		}
 
 	private:
-		void handleMessage(WebSocket::ref webSocket, boost::shared_ptr<JSONObject> message) {
+		void handleMessage(WebSocket::ref webSocket, std::shared_ptr<JSONObject> message) {
 			std::cout << "Receiving websocket message " << message->serialize() << std::endl;
 			if (sessions_->hasSession(webSocket)) {
 				// Unexpected message, but just ignore for this demo and reprocess it
 			}
 
-			boost::shared_ptr<JSONString> type = boost::dynamic_pointer_cast<JSONString>(message->getValues()["type"]);
+			std::shared_ptr<JSONString> type = std::dynamic_pointer_cast<JSONString>(message->getValues()["type"]);
 
 			if (!type || type->getValue() != "login") {
 				std::cout << "Not a login" << std::endl;
 				return;
 			} // Bad format
 
-			boost::shared_ptr<JSONString> user = boost::dynamic_pointer_cast<JSONString>(message->getValues()["user"]);
-			boost::shared_ptr<JSONString> password = boost::dynamic_pointer_cast<JSONString>(message->getValues()["password"]);
+			std::shared_ptr<JSONString> user = std::dynamic_pointer_cast<JSONString>(message->getValues()["user"]);
+			std::shared_ptr<JSONString> password = std::dynamic_pointer_cast<JSONString>(message->getValues()["password"]);
 
 			if (!user || !password || user->getValue() != "demo" || password->getValue() != "secret!") {
 				std::cout << "Login invalid" << std::endl;
@@ -121,15 +120,15 @@ class WebSockets {
 
 			std::cout << "Login successful" << std::endl;
 
-			boost::shared_ptr<DemoSession> session = boost::make_shared<DemoSession>(webSocket);
+			std::shared_ptr<DemoSession> session = std::make_shared<DemoSession>(webSocket);
 			std::string key = sessions_->addSession(session);
-			WebSocketHinter::ref hinter = boost::make_shared<WebSocketHinter>(webSocket);
+			WebSocketHinter::ref hinter = std::make_shared<WebSocketHinter>(webSocket);
 			hinters_.push_back(hinter);
 			webSocket->onClosed.connect(boost::bind(&WebSockets::handleWebSocketClosed, this, hinter));
 
-			boost::shared_ptr<JSONObject> reply = boost::make_shared<JSONObject>();
-			reply->set("type", boost::make_shared<JSONString>("login-success"));
-			reply->set("cookie", boost::make_shared<JSONString>("librestpp_session=" + key));
+			std::shared_ptr<JSONObject> reply = std::make_shared<JSONObject>();
+			reply->set("type", std::make_shared<JSONString>("login-success"));
+			reply->set("cookie", std::make_shared<JSONString>("librestpp_session=" + key));
 			std::cout << "Sending reply:" << reply->serialize() << std::endl;
 			hinter->send(reply);
 		}
@@ -142,19 +141,19 @@ class WebSockets {
 
 	private:
 		std::vector<WebSocketHinter::ref> hinters_;
-		boost::shared_ptr<DemoSessionCollection> sessions_;
+		std::shared_ptr<DemoSessionCollection> sessions_;
 
 };
 
-void handleConfigPostRequest(boost::shared_ptr<RESTRequest> request, Config* config, WebSockets* webSockets) {
-	boost::shared_ptr<JSONObject> json = request->getJSON();
+void handleConfigPostRequest(std::shared_ptr<RESTRequest> request, Config* config, WebSockets* webSockets) {
+	std::shared_ptr<JSONObject> json = request->getJSON();
 
-	boost::shared_ptr<JSONString> str;
+	std::shared_ptr<JSONString> str;
 
 	if (!!json) {
 		auto values = json->getValues();
 		auto nameJSON = values["name"];
-		str = boost::dynamic_pointer_cast<JSONString>(nameJSON);
+		str = std::dynamic_pointer_cast<JSONString>(nameJSON);
 	}
 	if (str) {
 		config->name_ = str->getValue();
@@ -177,26 +176,26 @@ int main(int argc, const char* argv[])
 	Config config;
 	config.name_ = "Demo";
 
-	boost::shared_ptr<DemoSessionCollection> sessions = boost::make_shared<DemoSessionCollection>();
+	std::shared_ptr<DemoSessionCollection> sessions = std::make_shared<DemoSessionCollection>();
 
 	WebSockets webSockets(sessions);
 
-	typedef SessionRESTHandler<boost::shared_ptr<DemoSession>> DemoSessionRestHandler;
+	typedef SessionRESTHandler<std::shared_ptr<DemoSession>> DemoSessionRestHandler;
 
-	boost::shared_ptr<DemoSessionRestHandler> configGetHandler = boost::make_shared<DemoSessionRestHandler>(sessions, [&config](boost::shared_ptr<DemoSession>, boost::shared_ptr<RESTRequest> request) {handleConfigGetRequest(request, &config);});
+	std::shared_ptr<DemoSessionRestHandler> configGetHandler = std::make_shared<DemoSessionRestHandler>(sessions, [&config](std::shared_ptr<DemoSession>, std::shared_ptr<RESTRequest> request) {handleConfigGetRequest(request, &config);});
 	server.addJSONEndpoint(PathVerb("/api/config", PathVerb::GET), configGetHandler);
 
-	boost::shared_ptr<DemoSessionRestHandler> configPostHandler = boost::make_shared<DemoSessionRestHandler>(sessions, [&config, &webSockets](boost::shared_ptr<DemoSession>, boost::shared_ptr<RESTRequest> request) {handleConfigPostRequest(request, &config, &webSockets);});
+	std::shared_ptr<DemoSessionRestHandler> configPostHandler = std::make_shared<DemoSessionRestHandler>(sessions, [&config, &webSockets](std::shared_ptr<DemoSession>, std::shared_ptr<RESTRequest> request) {handleConfigPostRequest(request, &config, &webSockets);});
 	server.addJSONEndpoint(PathVerb("/api/config", PathVerb::POST), configPostHandler);
 
 
-	boost::shared_ptr<JSONRESTHandler> cssHandler = boost::make_shared<MemoryFileHandler>("client/_build/styles.nonCached.css");
+	std::shared_ptr<JSONRESTHandler> cssHandler = std::make_shared<MemoryFileHandler>("client/_build/styles.nonCached.css");
 	server.addJSONEndpoint(PathVerb("/styles.nonCached.css", PathVerb::GET), cssHandler);
 
-	boost::shared_ptr<JSONRESTHandler> jsHandler = boost::make_shared<MemoryFileHandler>("client/_build/app.nonCached.js");
+	std::shared_ptr<JSONRESTHandler> jsHandler = std::make_shared<MemoryFileHandler>("client/_build/app.nonCached.js");
 	server.addJSONEndpoint(PathVerb("/app.nonCached.js", PathVerb::GET), jsHandler);
 
-	boost::shared_ptr<JSONRESTHandler> htmlHandler = boost::make_shared<MemoryFileHandler>("client/_build/index.html");
+	std::shared_ptr<JSONRESTHandler> htmlHandler = std::make_shared<MemoryFileHandler>("client/_build/index.html");
 	server.addDefaultGetEndpoint(htmlHandler);
 
 	server.onWebSocketConnection.connect(boost::bind(&WebSockets::handleNewWebSocket, &webSockets, _1));
